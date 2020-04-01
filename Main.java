1dfa4e788aa33126cacff13a1a2f6f226ed9e829
basecamper <john@john.john>
2020-04-01 20:02:40 +0200

    code-tester ok

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed");
          PandemieDatenbank db = new PandemieDatenbank();
+         db.add("20.12.2020",40);
+         db.add("1.12.2020",22);
+         db.add("1.11.2020",3);
+         db.savetoFile("i.txt");
+         db.loadFromFile("i.txt");
+        System.out.println(db.toString());
     }
 }
